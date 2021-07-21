8f72ccf831ca9489a0a87f3ee09d5f80088ecda3
Jinnyre Malazarte <jinnyrem@protonmail.com>
2021-07-21 22:20:30 +0100

    stock data scraper

--- stock_data_scraper.py
+++ stock_data_scraper.py
@@ -1,4 +1,5 @@
 import requests
+import os
 import pandas as pd
 from datetime import date, timedelta
 
@@ -6,7 +7,7 @@ from datetime import date, timedelta
 params = {
   'access_key': '087159d85393bf79ec34aea670edf7fe'
 }
-
+Your dedicated access key is: 77HAARG0F1BQY2GS
 api_result = requests.get('https://api.marketstack.com/v1/tickers/', stock, '/intraday/' , params)
 
 api_response = api_result.json()
@@ -18,14 +19,22 @@ access_key = '?access_key=087159d85393bf79ec34aea670edf7fe'
 
 
 df = pd.DataFrame()
+interval = '1min'
 
 start_date = date(2021, 6, 1)
-end_date = date(2021, 6, 31)
+end_date = date(2021, 6, 30)
 delta = timedelta(days=1)
 
 while start_date <= end_date:
     date_set = start_date.strftime("%Y-%m-%d")
-    test_result = requests.get('https://api.marketstack.com/v1/tickers/', stock, '/intraday/', date_set, access_key)
-    clean_data = pd.json_normalize(test_result, record_path=['data', 'intraday'], meta=[['data', 'name'], ['data', 'symbol']])
+    url = 'http://api.marketstack.com/v1/intraday/' + access_key + '/' + stock  + date_set + '/intraday' +  + '&interval=' +  interval + 
+    print(url)
+    test_result = requests.get(url)
+    clean_data =  pd.json_normalize(test_result, record_path=['data', 'intraday'], meta=[['data', 'name'], ['data', 'symbol']])
     df.append(clean_data)
     start_date += delta
+
+location =r'C:\Users\Jin\Dropbox\2020 Course\Modules\Project'
+df.to_csv(os.path.join(location,r'GME.csv'))
+
+
