62554dda0f3b76d56c8aa59d5f4e5a932e2ac10c
Jinnyre Malazarte <jinnyrem@protonmail.com>
2021-07-26 19:39:34 +0100

    pseudonymised, cleaned columns not needed, converted time to local irish time

--- data_clean_up.R
+++ data_clean_up.R
@@ -4,8 +4,8 @@ library(foreach)
 library(doParallel)
 library(tidyverse)
 library(lubridate)
-install.packages("lubridate")
-install.packages('Rcpp')
+library(data.table)
+library(digest)
 
 load_dot_env("config.env")
 location = Sys.getenv("location")
@@ -16,59 +16,22 @@ getwd()
 stock_test = read.csv("AAPL.csv")
 tweets = read.csv("tweets.csv")
 
-# editing timezone to match with the tweets
-typeof(stock_test$time)
-typeof(tweets$Datetime)
-
-pb.txt <- "2009-06-03 08:00+00:00"
-pb.date <- as.POSIXct(pb.txt, tz="Atlantic/Reykjavik")
-pb.date
-format(pb.date, tz="Europe/Dublin",usetz=TRUE)
-test = format(pb.date, tz="America/New_York",usetz=TRUE)
-
-# Europe/Dublin
-# America/New_York
-# Atlantic/Reykjavik
-
-head(stock_test$time)
-head(tweets$Datetime)
-
-test_tweets = tweets$Datetime
-test_run = head(test_tweets)
-pb.txt <- "2009-06-03 08:00+00:00"
-pattern = "\\+00:00"
-pb.txt.removed = str_remove_all(test_run[1], pattern)
-test_run[1]
-pb.txt.removed
-converted_times = {}
-
-for (x in test_run) {
-  correct_time = str_remove(x, pattern)
-  converted_times =  rbind(converted_times,correct_time)
-}
-converted_times
-
-test_run[1]
-for (i in 1:length(test_run)){
-  x = str_remove(test_run[i], pattern)
-  test_run[i] = x
-}
-test_run
-typeof(test_run)
 
 #setup parallel backend to use many processors
+# remove the pattern "+00:00" from the time
 cores=detectCores()
 cl <- makeCluster(cores[1]-1) #not to overload your computer
 registerDoParallel(cl)
 
 test_run2 = tweets
 length(test_run2)
+pattern = "\\+00:00"
+
 test_run2$Datetime = foreach(i=1:length(test_run2$Datetime), .packages = "stringr") %dopar% {
                         x = str_remove(test_run2$Datetime[i], pattern)
                         test_run2$Datetime[i] = x
                       }
-head(test_run2$Datetime)
-sapply(test_run2, class)
+
 test_run2$Datetime = unlist(test_run2$Datetime)
 write.csv(test_run2, "tweets_time_cleaned.csv", row.names = FALSE)
 head(test_run2)
@@ -92,11 +55,7 @@ write.csv(cleaned, "tweets_time_cleaned_trimmed.csv", row.names = FALSE)
 
 test_run3 = read.csv("tweets_time_cleaned_trimmed.csv")
 
-# time_in_UTC = lubridate::ymd_hms(test_run3$Datetime[i])
-# converted_to_IST = lubridate::with_tz(time_in_UTC, "Europe/Dublin")
-# numeric_date = as.numeric(as.character(converted_to_IST))
-# numeric_date_converted = as.POSIXct(numeric_date, origin = "1950-01-01") 
-# test_run3$Datetime[i] = numeric_date_converted
+
 test_run4 = test_run3
 cores=detectCores()
 cl <- makeCluster(cores[1]-1) #not to overload your computer
@@ -111,7 +70,8 @@ test_run4$Datetime = foreach(i=1:length(test_run4$Datetime), .packages = "lubrid
 }
 time_it_took <- proc.time()-start
 # user  system elapsed 
-# 139.98   20.64  282.19 
+# 139.98   20.64  282.19
+
 time_it_took
 #stop cluster
 stopCluster(cl)
@@ -153,9 +113,22 @@ stopCluster(cl)
 test_run5$time = do.call("c", test_run5$time)
 write.csv(test_run5, "AAPL_time_converted.csv", row.names = FALSE)
 
-typeof(test_run5$time)
-head(test_run5)
-head(stock_test)
+# apply this to all stock csvs
 
-OlsonNames()
 
+###### pseudonymisation of usernames
+tweets = read.csv("tweets_time_cleaned_trimmed_time_converted.csv")
+
+test1 = tweets
+
+digest_seed = Sys.getenv(seed)
+
+typeof(test1$Username)
+
+pseudonymize_names = sapply(test1$Username, digest, algo="crc32", serialize=FALSE)
+
+test1$Username = pseudonymize_names
+
+test1$Username
+
+write.csv(test1, "tweets_cleaned_pseudonymized.csv", row.names = FALSE)
